e4e4813c01def2dc8796b4d964ae1bd50605fdc4
Shivanshu Singh <shivanshusom@gmail.com>
2025-08-06 22:28:12 +0530

    Reduce white space between sticky headers and main content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <main className="pt-32">
+      <main className="pt-20">
         {children}
       </main>
     </div>
